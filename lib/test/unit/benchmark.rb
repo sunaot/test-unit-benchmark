c718350181d51c2134f180e2cda95a211b38c913
sunaot <sunao.tanabe@gmail.com>
2019-06-15 03:29:56 +0900

    Initial files

---
+++ lib/test/unit/benchmark.rb
@@ -0,0 +1,10 @@
+require "test/unit/benchmark/version"
+
+module Test
+  module Unit
+    module Benchmark
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/test/unit/benchmark/version.rb
@@ -0,0 +1,7 @@
+module Test
+  module Unit
+    module Benchmark
+      VERSION = "0.1.0"
+    end
+  end
+end
